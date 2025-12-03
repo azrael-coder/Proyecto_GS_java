692279298e4a72f2a4a932a23e54b40715edc607
azrael-coder <israelgonzalez050908@gmail.com>
2025-12-03 19:55:39 +0100

    Encapsulacion

--- src/POO/CuentaBancaria.java
+++ src/POO/CuentaBancaria.java
@@ -1,47 +1,76 @@
 package POO;
+
 public class CuentaBancaria {
-    private final String titular;
+    private String titular;
     private double saldo;
+    private final int numCuenta;
 
-    public CuentaBancaria(String titular, double saldoInicial) {
+    CuentaBancaria (String titular, double saldoInicial, int numCuenta){
         this.titular = titular;
-        this.saldo = saldoInicial;
+        this.numCuenta = numCuenta;
+
+        if (saldoInicial > 0){
+            this.saldo = saldoInicial;
+        } else {
+            System.out.println("Saldo invalido");
+        }
     }
 
-
-
-    public void depositar( double cantidad){
-        if (cantidad > 0)
+    public boolean depositar( double cantidad){
+        if (cantidad > 0) {
             saldo += cantidad;
+            return true;
+        }else{
+            System.out.println("cantidad invalida");
+            return false;
+        }
     }
 
-    public void retirar(double cantidad){
-        if (cantidad > 0 && saldo >= cantidad)
+    public boolean retirar(double cantidad) {
+        if (cantidad > 0 && saldo >= cantidad){
             saldo -= cantidad;
+        return true;
+        }else {
+            System.out.println("Se ha rechazado su retiro");
+            return false;
+        }
     }
 
     public void mostrarInfo(){
         System.out.println("Titular: "+titular);
         System.out.println("Saldo: "+saldo);
+        System.out.println("Numero de cuenta: "+numCuenta);
         System.out.println("----------");
     }
 
-    public boolean numerosRojos() {
-        if (saldo < 0)
-            return  true;
-        else
-            return false;
 
-    }
-
-    public void transferir (double cantidad, CuentaBancaria otraCuenta ){
-        if (cantidad > 0 && saldo >= cantidad) {
+    public boolean transferir (double cantidad, CuentaBancaria otraCuenta ){
+        if (cantidad > 0 && saldo >= cantidad && otraCuenta != null) {
             this.retirar(cantidad);
             otraCuenta.depositar(cantidad);
+            return true;
         }else
-            System.out.println("Saldo insuficiente");
+            System.out.println("Transferencia rechazada");
+        return false;
+
+    }
+    //MOSTRAR SALDO
+    public double getSaldo(){
+        return saldo;
     }
 
+    //MOSTRAT TITULAR
+    public String getTitular(){
+        return titular;
+    }
+    //MODFICAR EL TITULAR
+    public void setTitular(String titular){
+        this.titular = titular;
+    }
+    //MSTRAR EL NUMERO DE CUENTA
+    public int getNumCuenta(){
+        return numCuenta;
+    }
 
 
 }
--- src/POO/Main.java
+++ src/POO/Main.java
@@ -2,16 +2,23 @@ package POO;
 public class Main {
     public static void main(String[] args){
 
-        CuentaBancaria cuenta = new CuentaBancaria("israel", 0);
-
-        CuentaBancaria cuenta2 = new CuentaBancaria("sal", 0);
-
+        CuentaBancaria cuenta1 = new CuentaBancaria("israel", 10, 1);
+        CuentaBancaria cuenta2 = new CuentaBancaria("sal", 50, 2);
 
 
+        cuenta1.depositar(500);
 
-        cuenta.mostrarInfo();
-        cuenta2.mostrarInfo();
 
+        if (cuenta1.transferir(500, cuenta2)){
+            System.out.println("Transferencia realizada con exito");
+            System.out.println("Saldo actual: "+cuenta1.getSaldo());
+            System.out.println("----------");
+        }else{
+            System.out.println("Transferencia fallida");
+        }
+
+        cuenta1.retirar(10);
+        System.out.println("Saldo actual: "+cuenta1.getSaldo());
 
 
     }
