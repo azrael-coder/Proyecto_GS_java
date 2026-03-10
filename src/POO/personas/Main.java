ea3a85d347c18e7215c343a6d8a9e46f0ff97e2d
azrael <israelgonzalez050908@gmail.com>
2026-03-10 09:46:14 +0100

    done

--- src/POO/personas/Main.java
+++ src/POO/personas/Main.java
@@ -3,12 +3,21 @@ package POO.personas;
 public class Main {
     public static void main(String[] args) {
         Estudiante estudiante1 = new Estudiante("Nombre", "Gonzalez",889);
+        Jefe j1 = new Jefe("Boss", "Mandamas",4000);
 
-        estudiante1.pagarbeca();
+        j1.prepararCharla("Te vendo mi curso online sobre trading");
+        j1.darCharla();
+
+        System.out.println(j1);
+        j1.igresarDinero(300.5);
+        System.out.println(j1);
 
 
-        estudiante1.estudiar(6);
+        //estudiante1.pagarbeca();
 
-        System.out.println(estudiante1);
+
+        //estudiante1.estudiar(6);
+
+        //System.out.println(estudiante1);
     }
 }
